206900e4d845c4e75c08c8d75a74755bd12070c7
Amanda Hussey <amanda.k.hussey@gmail.com>
2025-04-04 15:34:50 -0600

    more spacing + new icons

--- ben-g-therapy.com/src/App.tsx
+++ ben-g-therapy.com/src/App.tsx
@@ -2,13 +2,17 @@ import { Link, Stack, ThemeProvider, Typography } from "@mui/material";
 
 import ben from "./assets/ben.jpg";
 import benSilly from "./assets/ben-silly.jpg";
+import mentalHealthIcon from "./assets/mental-health.png";
+import relievedIcon from "./assets/relieved.png";
+import therapyIcon from "./assets/therapy.png";
+
 import "./App.css";
 import { theme } from "./theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Stack justifyContent="center" alignItems="center">
+      <Stack justifyContent="center" alignItems="center" maxWidth={1200}>
         {/* Section 1 */}
         <Stack
           direction="row"
@@ -16,7 +20,7 @@ function App() {
           spacing={4}
           minHeight="100vh"
         >
-          <Typography variant="h4" textAlign="left" maxWidth={600}>
+          <Typography variant="h4" textAlign="left">
             How would it feel to be heard, understood, and supported through
             hard times in your life?
           </Typography>
@@ -39,44 +43,64 @@ function App() {
 
         {/* Section 2 */}
         <Stack
-          direction="row"
           alignItems="center"
           justifyContent="center"
           spacing={4}
           minHeight="100vh"
+          pb={8}
         >
-          <img src={benSilly} style={{ height: 300, borderRadius: "50%" }} />
-          <Stack spacing={3}>
+          <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="center"
+            spacing={4}
+          >
+            <img src={benSilly} style={{ height: 400, borderRadius: "50%" }} />
+            <Stack spacing={3} maxWidth={600}>
+              <Typography variant="h5" textAlign="left">
+                Imagine moving through life with the confidence to be yourself,
+                not hiding or being concerned about how you are being perceived.
+              </Typography>
+            </Stack>
+          </Stack>
+
+          <Stack direction="row" alignItems="center" spacing={6}>
             <Typography variant="h5" textAlign="left">
-              Imagine moving through life with the confidence to be yourself,
-              not hiding or being concerned about how you are being perceived.
-              What if your mind was an asset instead of your worst enemy!
+              What if your mind was an asset instead of your worst enemy?
             </Typography>
-            <Typography variant="h5" textAlign="left">
-              You can embody joy, embrace life and face challenges you thought
-              impossible.
-            </Typography>
+            <img src={mentalHealthIcon} />
+          </Stack>
+
+          <Stack direction="row" alignItems="center" spacing={6}>
+            <img src={relievedIcon} />
+
+            <Stack spacing={5}>
+              <Typography variant="h5" textAlign="left">
+                You can embody joy, embrace life and face challenges you thought
+                impossible.
+              </Typography>
+
+              <Typography variant="h5" textAlign="left">
+                Through a multi-diciplinary approach leaning heavily on EMDR and
+                CBT, I am here to support you and provide meaningful methods for
+                change into your life.
+              </Typography>
+            </Stack>
           </Stack>
         </Stack>
 
-        {/* Section 3 */}
         <Stack
+          spacing={2}
           alignItems="center"
           justifyContent="center"
-          minHeight="100vh"
-          spacing={4}
+          minHeight="50vh"
+          pb={8}
         >
-          <Typography variant="h5">
-            Through a multi-diciplinary approach leaning heavily on EMDR and
-            CBT, I am here to support you and provide meaningful methods for
-            change into your life.
-          </Typography>
-
+          <img src={therapyIcon} />
           <Typography variant="h4">
             Don't let another day go by without being seen, heard, and
-            supported!
+            supported.
           </Typography>
-
           <Typography variant="h5">
             Give me a call to set up your first appointment today!
           </Typography>
