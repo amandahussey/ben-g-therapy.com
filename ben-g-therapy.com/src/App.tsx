427d678f15df566ae45f7e3a3cc37614dc565c88
Amanda Hussey <amanda.k.hussey@gmail.com>
2025-04-04 14:36:30 -0600

    styling + new cropped pics

--- ben-g-therapy.com/src/App.tsx
+++ ben-g-therapy.com/src/App.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 
 import ben from "./assets/ben.jpg";
 import benSilly from "./assets/ben-silly.jpg";
@@ -6,30 +6,32 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div>
-        <img
-          src={ben}
-          alt="Vite logo"
-          style={{ height: 200, borderRadius: "50%" }}
-        />
-        <h1>Ben Green</h1>
-        <img
-          src={benSilly}
-          alt="React logo"
-          style={{ height: 200, borderRadius: "50%" }}
-        />
-      </div>
+    <Stack justifyContent="center" alignItems="center">
+      <Stack direction="row" alignItems="center" spacing={4}>
+        <Typography variant="h4" textAlign="left" maxWidth={600}>
+          How would it feel to be heard, understood, and supported through hard
+          times in your life?
+        </Typography>
+        <Stack alignItems="center" justifyContent="center" spacing={3}>
+          <img src={ben} style={{ height: 400, borderRadius: "50%" }} />
+          <Typography variant="h5">Ben Green, LCPP</Typography>
+        </Stack>
+      </Stack>
 
-      <Typography variant="h4">
-        How would it feel to be heard, understood, and supported through hard
-        times in your life?
-      </Typography>
-      <Typography variant="h5">
-        Imagine moving through life with the confidence to be yourself, not
-        hiding or being concerned about how you are being perceived. What if
-        your mind was an asset instead of your worst enemy!
-      </Typography>
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+        spacing={4}
+      >
+        <Typography variant="h5">
+          Imagine moving through life with the confidence to be yourself, not
+          hiding or being concerned about how you are being perceived. What if
+          your mind was an asset instead of your worst enemy!
+        </Typography>
+        <img src={benSilly} style={{ height: 200, borderRadius: "50%" }} />
+      </Stack>
+
       <Typography variant="h6">
         You can embody joy, embrace life and face challenges you thought
         impossible. Through a multi-diciplinary approach leaning heavily on EMDR
@@ -37,7 +39,7 @@ function App() {
         change into your life. Don't let another day go by without being seen,
         heard, and supported!
       </Typography>
-    </>
+    </Stack>
   );
 }
 
